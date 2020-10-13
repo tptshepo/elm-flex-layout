5e79940844dd8783708f290855367a4ae4ae2e08
Tshepo Mgaga <tshepo.mgaga@gmail.com>
2020-10-13 23:42:30 +0200

    Updated LayoutAlign example

--- src/Examples/LayoutAlign.elm
+++ src/Examples/LayoutAlign.elm
@@ -5,14 +5,15 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 
 
-blockStyles : String -> List (Attribute msg)
-blockStyles backgroundColor =
-    [ style "min-height" "75px"
-    , style "min-width" "50px"
+blockStyles : String -> String -> String -> List (Attribute msg)
+blockStyles backgroundColor minWidth minHeight =
+    [ style "min-height" minHeight
+    , style "min-width" minWidth
     , style "border-radius" "3px"
     , style "color" "#fff"
     , style "text-align" "center"
-    , style "padding" "5px"
+    , style "padding" "8px"
+    , style "box-shadow" "0 2px 5px 0 rgba(52,47,51,.63)"
     , style "font-size" "14px"
     , style "font-family" "Roboto, Helvetica Neue, sans-serif"
     , style "background-color" backgroundColor
@@ -21,20 +22,60 @@ blockStyles backgroundColor =
 
 main : Html msg
 main =
+    let
+        direction : Direction
+        direction =
+            Flex.column
+    in
     div
-        ([ style "height" "200px"
+        ([ style "height" "402px"
          , style "border" "1px solid gray"
          , style "margin" "50px"
          ]
-            ++ fxLayout Flex.row Flex.start Flex.stretch
+            ++ fxLayout direction Flex.spaceAround Flex.stretch
         )
         [ div
-            ([] ++ blockStyles "#009688")
+            ([] ++ blockStyles "#009688" "75px" "50px")
             [ text "1" ]
         , div
-            ([] ++ blockStyles "#3949ab")
+            ([]
+                ++ blockStyles
+                    "#3949ab"
+                    (if direction == Flex.row then
+                        "75px"
+
+                     else
+                        "125px"
+                    )
+                    (if direction == Flex.row then
+                        "100px"
+
+                     else
+                        "50px"
+                    )
+            )
             [ text "2" ]
         , div
-            ([] ++ blockStyles "#9c27b0")
+            ([] ++ blockStyles "#9c27b0" "75px" "50px")
             [ text "3" ]
+        , div
+            ([]
+                ++ blockStyles "#b08752"
+                    (if direction == Flex.row then
+                        "75px"
+
+                     else
+                        "100px"
+                    )
+                    (if direction == Flex.row then
+                        "75px"
+
+                     else
+                        "50px"
+                    )
+            )
+            [ text "4" ]
+        , div
+            ([] ++ blockStyles "#5ca6b0" "75px" "50px")
+            [ text "5" ]
         ]
