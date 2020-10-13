c14916b67696fa83c7a324ec72ce6b81dc21977d
Tshepo Mgaga <tshepo.mgaga@gmail.com>
2020-10-13 23:03:44 +0200

    Updated LayoutAlign example

--- src/Examples/LayoutAlign.elm
+++ src/Examples/LayoutAlign.elm
@@ -5,25 +5,36 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 
 
+blockStyles : String -> List (Attribute msg)
+blockStyles backgroundColor =
+    [ style "min-height" "75px"
+    , style "min-width" "50px"
+    , style "border-radius" "3px"
+    , style "color" "#fff"
+    , style "text-align" "center"
+    , style "padding" "5px"
+    , style "font-size" "14px"
+    , style "font-family" "Roboto, Helvetica Neue, sans-serif"
+    , style "background-color" backgroundColor
+    ]
+
+
 main : Html msg
 main =
-    div ([] ++ fxLayout Flex.Row Flex.Start Flex.Stretch)
+    div
+        ([ style "height" "200px"
+         , style "border" "1px solid gray"
+         , style "margin" "50px"
+         ]
+            ++ fxLayout Flex.row Flex.start Flex.stretch
+        )
         [ div
-            [ style "height" "100px"
-            , style "width" "100px"
-            , style "background-color" "blue"
-            ]
-            []
+            ([] ++ blockStyles "#009688")
+            [ text "1" ]
         , div
-            [ style "height" "100px"
-            , style "width" "100px"
-            , style "background-color" "green"
-            ]
-            []
+            ([] ++ blockStyles "#3949ab")
+            [ text "2" ]
         , div
-            [ style "height" "100px"
-            , style "width" "100px"
-            , style "background-color" "yellow"
-            ]
-            []
+            ([] ++ blockStyles "#9c27b0")
+            [ text "3" ]
         ]
